d40ebe2760c16b7290c1c9725eaf57b2d58dbff4
Vanessa Cecília <cecilia.roque05@gmail.com>
2025-07-23 17:41:11 -0300

    subindo projeto

---
+++ projeto-magickai/src/js/index-refatorado.js
@@ -0,0 +1,50 @@
+const botaoFiltrar = document.querySelector(".btn-filtrar");
+
+botaoFiltrar.addEventListener("click", aplicarFiltros);
+
+function aplicarFiltros() {
+    const categoriaSelecionada = pegarCategoriaSelecionada();
+    const precoMaximoSelecionado = pegarPrecoMaximoSelecionado();
+    const cartas = pegarCartas();
+
+    cartas.forEach(function (carta) {
+        const categoriaValida = verificarCategoria(cartas, categoriaSelecionada);
+        const precoValido = verificarPreco(carta, precoMaximoSelecionado);
+
+        const deveMostrarCarta = categoriaValida && precoValido;
+
+        mostrarOuEsconderCarta(carta, deveMostrarCarta);
+    });
+}
+
+function pegarCategoriaSelecionada() {
+    return document.querySelector("#categoria").value;
+}
+
+function pegarPrecoMaximoSelecionado() {
+    return document.querySelector("#preco").value;
+}
+
+function pegarCartas() {
+    return document.querySelectorAll(".carta");
+}
+
+function verificarCategoria(carta, categoriaSelecionada) {
+    const categoriaCarta = carta.dataset.categoria;
+    return categoriaSelecionada === "" || categoriaSelecionada.toLowerCase() === categoriaCarta.toLowerCase();
+}
+
+function verificarPreco(carta, precoMaximoSelecionado) {
+    const precoCarta = carta.dataset.preco;
+    return precoMaximoSelecionado === "" || parseFloat(precoCarta) <= parseFloat(precoMaximoSelecionado);
+}
+
+function mostrarOuEsconderCarta(carta, deveMostrarCarta) {
+    if (deveMostrarCarta) {
+        carta.classList.add("mostrar");
+        carta.classList.remove("esconder");
+    } else {
+        carta.classList.remove("mostrar");
+        carta.classList.add("esconder");
+    }
+}
